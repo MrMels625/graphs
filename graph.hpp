14742610831610afdae6aebca493e824887a4e0d
MrMels625 <MrMels625@gmail.com>
2026-07-18 14:20:01 +0300

    Graph basic methods added & tested
    new methods for Graph class:
    - add/remove edge/vertex
    - degree of certain vertex
    - clear, empty
    - vertices, edges count
    
    tests for Graph:
    - simple Graph with 3 elements adding and clearing

--- graph.hpp
+++ graph.hpp
@@ -2,16 +2,97 @@
 #define GRAPH_HPP
 
 #include <unordered_map>
-#include <vector>
+#include <list>
 
 template< class T >
 class Graph
 {
 public:
+  void add_edge(const T&, const T&);
+  void add_vertex(const T&);
+
+  void remove_edge(const T&, const T&);
+  void remove_vertex(const T&);
+
+  size_t degree(const T&) noexcept;
+
+  void clear() noexcept;
+
+  size_t vertices() const noexcept;
+  size_t edges() const noexcept;
+  bool empty() const noexcept;
 
 private:
-  std::unordered_map< T, std::vector< T > > adjacent_list;
+  std::unordered_map< T, std::list< T > > adjacent_list;
 };
+
+template< class T >
+void Graph< T >::add_edge(const T& first, const T& second)
+{
+  adjacent_list[first].push_back(second);
+  if (first != second)
+  {
+    adjacent_list[second].push_back(first);
+  }
+}
+
+template< class T >
+void Graph< T >::add_vertex(const T& vertex)
+{
+  adjacent_list[vertex];
+}
+
+template< class T >
+void Graph< T >::remove_edge(const T& first, const T& second)
+{
+  if (adjacent_list.contains(first) && adjacent_list.contains(second))
+  {
+    adjacent_list[first].remove(second);
+    if (first != second)
+    {
+      adjacent_list[second].remove(first);
+    }
+  }
+}
+
+template< class T >
+void Graph< T >::remove_vertex(const T& vertex)
+{
+  if (adjacent_list.contains(vertex))
+  {
+    adjacent_list.erase(vertex);
+  }
+}
+
+template< class T >
+size_t Graph< T >::degree(const T& vertex) noexcept
+{
+  return adjacent_list.contains(vertex) ? adjacent_list[vertex].size() : 0ull;
+}
+
+template< class T >
+void Graph< T >::clear() noexcept
+{
+  adjacent_list.clear();
+}
+
+template< class T >
+size_t Graph< T >::vertices() const noexcept
+{
+  return adjacent_list.size();
+}
+
+template< class T >
+size_t Graph< T >::edges() const noexcept
+{
+  return 0ull;
+}
+
+template< class T >
+bool Graph< T >::empty() const noexcept
+{
+  return adjacent_list.empty();
+}
 
 #endif
 
--- test-graphs.cpp
+++ test-graphs.cpp
@@ -1,8 +1,44 @@
 #include <boost/test/unit_test.hpp>
 #include "graph.hpp"
 
-BOOST_AUTO_TEST_CASE(test)
+BOOST_AUTO_TEST_CASE(basic_tests)
 {
-  BOOST_TEST(true);
+  Graph< int > gr1;
+  BOOST_TEST(gr1.empty());
+  BOOST_TEST(gr1.vertices() == 0ull);
+  BOOST_TEST(gr1.edges() == 0ull);
+  BOOST_TEST(gr1.degree(1) == 0ull);
+
+  gr1.add_vertex(1);
+  BOOST_TEST(!gr1.empty());
+  BOOST_TEST(gr1.vertices() == 1ull);
+  BOOST_TEST(gr1.edges() == 0ull);
+  BOOST_TEST(gr1.degree(1) == 0ull);
+
+  gr1.add_vertex(3);
+  BOOST_TEST(gr1.vertices() == 2ull);
+
+  gr1.add_edge(1, 3);
+  BOOST_TEST(gr1.edges() == 1ull);
+  BOOST_TEST(gr1.degree(1) == 1ull);
+  BOOST_TEST(gr1.degree(3) == 1ull);
+
+  gr1.add_vertex(3);
+  BOOST_TEST(gr1.vertices() == 2ull);
+
+  gr1.add_edge(1, 3);
+  BOOST_TEST(gr1.edges() == 1ull);
+  BOOST_TEST(gr1.degree(1) == 1ull);
+  BOOST_TEST(gr1.degree(3) == 1ull);
+
+  gr1.add_vertex(2);
+  BOOST_TEST(gr1.vertices() == 3ull);
+
+  gr1.add_edge(3, 2);
+  gr1.add_edge(3, 3);
+  BOOST_TEST(gr1.edges() == 3ull);
+
+  gr1.clear();
+  BOOST_TEST(gr1.empty());
 }
 
